a5778387d375680100d617a452f7d413e602f340
Soler Barriocanal, Jordi <Jordi.Soler@gft.com>
2022-07-19 14:30:42 +0200

    contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { AppComponent } from './app.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ExperienceComponent } from './components/experience/experience.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
-import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
@@ -16,9 +14,7 @@ import { ProjectsComponent } from './components/projects/projects.component';
     MainComponent,
     AboutMeComponent,
     ExperienceComponent,
-    ProjectsComponent,
     ContactComponent,
-    FooterComponent,
   ],
   imports: [BrowserModule, AngularFullpageModule],
   providers: [],
