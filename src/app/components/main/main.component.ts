52c64b9ce103c890c3b22e046545564e8cd50fea
Soler Barriocanal, Jordi <Jordi.Soler@gft.com>
2022-07-19 10:11:46 +0200

    update porfolio

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -10,9 +10,18 @@ export class MainComponent implements OnInit {
 
   ngOnInit(): void {
     // Some random colors
-    const colors = ['#3CC157', '#2AA7FF', '#1B1B1B', '#FCBC0F', '#F85F36'];
+    const colors = [
+      '#3CC157',
+      '#2AA7FF',
+      '#1B1B1B',
+      '#FCBC0F',
+      '#F85F36',
+      '#067ea3',
+      '#616161',
+      '#04c2c9',
+    ];
 
-    const numBalls = 50;
+    const numBalls = 80;
     const balls = [];
 
     for (let i = 0; i < numBalls; i++) {
