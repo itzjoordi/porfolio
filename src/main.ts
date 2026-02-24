345e53aabf9f32ab55c39f55655020b545af66da
Jordi Soler <jordi.soler@gft.com>
2026-02-24 18:03:38 +0100

    chore: update angular 21

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, BrowserAnimationsModule)],
+  providers: [provideAnimationsAsync()],
 }).catch((err) => console.error(err));
